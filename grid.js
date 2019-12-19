8c60c2761007b9739c1d04b7bfd98379aef0d9d8
Cynthia Ware <cynthiaw1989@gmail.com>
2019-12-19 18:38:20 -0500

    completly refactor.  main functionality working. add clear function.  fix reset function

--- grid.js
+++ grid.js
@@ -1,53 +1,100 @@
-let rowTotal = 16;
-const gridContainer = document.getElementById('container');
-let gridTotal = rowTotal * rowTotal;
-let span = document.getElementsByClassName("gridSize");
-let resetBtn = document.querySelector('#resetBtn');
-let cell = document.getElementsByClassName("cell");
-//sets display for size of grid to row total
-function writeGridSize(){
-    for (i=0; i<span.length; i++){
-        span[i].textContent=rowTotal;
+//function that runs on load
+initial(16);
+function initial(gridSize){
+    drawGrid(gridSize);
+    createListeners();
+    const resetBtn = document.querySelector('#resetBtn');
+    resetBtn.addEventListener('click',resetGrid);
+    const clearBtn = document.querySelector('#clearBtn');
+    clearBtn.addEventListener('click',clearGrid);
+    const colorizeBtn = document.querySelector('#colorizeButton');
+    colorizeBtn.addEventListener('click',colorize);
+    //initial value for cellcolor
+    let cellColor = '#000000';
+    writeColor(cellColor);
+    //initial value for brightness, can be 0-255
+    let brightness = 220;
+    writeBrightness(brightness);
+    writeGridSize(gridSize);
+}
+//draws grid
+function drawGrid(gridSize){
+    let gridContainer = document.getElementById('container');
+    //sets column layout to one fraction per grid size
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    //creates one cell per gridSize squared
+    for(let i = 0; i < gridSize ** 2; i++){
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        gridContainer.appendChild(cell);
     }
 }
-//deletes cells withi grid container
-function deleteCells(){
-    while(gridContainer.firstChild){
-        gridContainer.removeChild(gridContainer.firstChild);
-        console.log("removed cell");
-    }
+//creates listeners for cells (hover effect)
+function createListeners(){
+    let cells = getCells();
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseenter', () => {
+            cell.classList.remove('clear');
+            cell.classList.add("marked");
+        });
+    });
+    cells.forEach((cell) => {
+        cell.addEventListener('click', () => {
+            cell.classList.add('clear');
+        })
+    })
 }
-//draws new grid
-function drawGrid(){
-    gridContainer.style.display = 'grid';
-    gridContainer.style.gridTemplateRows = `repeat(rowTotal, 10px)`;
-    gridContainer.style.gridTemplateColumns = `repeat(rowTotal, 10px)`;
-    let row = 1;
-    let column = 1;
-        for (let i=1; i <= gridTotal; i++){
-            cell = document.createElement('div');
-            cell.style.border = '1px solid black';
-            cell.className= "cell";
-            cell.style.gridRow = row;
-            cell.style.gridColumn = column;
-            cell.textContent = " ";
-            column += 1;
-                if (column == rowTotal +1){
-                    row += 1;
-                    column = 1;
-                }
-            gridContainer.appendChild(cell);
-        }
-    writeGridSize();
+function colorize(brightness){
+    cells = getCells();
+    cells.forEach((cell) => {
+        marked.style.backgroundColor = `${randomColor(brightness)}`;
+    });
 }
+//function that runs on reset button to remove cells and prompt for new grid
 function resetGrid(){
-    rowTotal = prompt("New grid size?");
-    deleteCells();
-    drawGrid();
-    writeGridSize();
+    cells = getCells();
+    cells.forEach((cell) => {
+        cell.parentNode.removeChild(cell);
+    });
+    initial(prompt("Grid Size?", 16));
 }
-drawGrid();
-//gives me console.log for mouseover
-cell.addEventListener("click", console.log("Mouse Entered cell"));
-//resets grid on btn click
-resetBtn.addEventListener("click", resetGrid);
+//function that removes marked class from all cells
+function clearGrid(){
+    cells = getCells();
+    cells.forEach((cell) => {
+        cell.classList.remove('marked');
+    });
+}
+//function that writes the size of the current grid in HTML display
+function writeGridSize(gridSize){
+    let displaySize = document.getElementsByClassName("displaySize");
+    for (i = 0; i < displaySize.length; i++){
+        displaySize[i].textContent = gridSize;
+    }
+}
+//function that writes color
+function writeColor(cellColor){
+    let displayColor = document.getElementById("displayColor");
+    displayColor.textContent = cellColor;
+}
+//function that writes Brightness
+function writeBrightness(brightness){
+    let displayBrightess = document.getElementById("displayBrightness");
+    displayBrightess.textContent = brightness;
+}
+//function that writes opacity
+//function because I'm tired of typing this out
+function getCells(){
+    return document.querySelectorAll(".cell");
+}
+
+//function for a random color generation
+function randomColor(brightness){
+    function randomChannel(brightness){
+      var r = 255-brightness;
+      var n = 0|((Math.random() * r) + brightness);
+      var s = n.toString(16);
+      return (s.length==1) ? '0'+s : s;
+    }
+    return '#' + randomChannel(brightness) + randomChannel(brightness) + randomChannel(brightness);
+  }
